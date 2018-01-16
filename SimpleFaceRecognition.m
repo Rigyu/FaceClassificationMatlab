871e81d6ecce89ba0ad70178c2bfa78852a0c932
R Rodriguez <r.rodriguez@esiee.fr>
2018-01-16 20:31:32 +0100

    Modification to use with "PeopleFace"
    Modified MATLAB code to use with PeopleFace database

--- SimpleFaceRecognition.m
+++ SimpleFaceRecognition.m
@@ -1,7 +1,8 @@
 %% Simple Face Recognition Example
 %  Copyright 2014-2015 The MathWorks, Inc.
+
 %% Load Image Information from ATT Face Database Directory
-faceDatabase = imageSet('FaceDatabaseATT','recursive');
+faceDatabase = imageSet('PeopleFace','recursive');
 
 %% Display Montage of First Face
 figure;
@@ -22,17 +23,18 @@ diff = zeros(1,9);
 %% Split Database into Training & Test Sets
 [training,test] = partition(faceDatabase,[0.8 0.2]);
 
-
 %% Extract and display Histogram of Oriented Gradient Features for single face 
-person = 5;
+person = 10;
 [hogFeature, visualization]= ...
     extractHOGFeatures(read(training(person),1));
 figure;
-subplot(2,1,1);imshow(read(training(person),1));title('Input Face');
-subplot(2,1,2);plot(visualization);title('HoG Feature');
+subplot(1,2,1);imshow(read(training(person),1));title('Input Face');
+subplot(1,2,2);plot(visualization);title('HoG Feature');
+
+export_fig HoGFeatures.png
 
 %% Extract HOG Features for training set 
-trainingFeatures = zeros(size(training,2)*training(1).Count,4680);
+trainingFeatures = zeros(size(training,2)*training(1).Count,34596);
 featureCount = 1;
 for i=1:size(training,2)
     for j = 1:training(i).Count
@@ -45,8 +47,6 @@ end
 
 %% Create 40 class classifier using fitcecoc 
 faceClassifier = fitcecoc(trainingFeatures,trainingLabel);
-
-
 %% Test Images from Test Set 
 person = 1;
 queryImage = read(test(person),1);
@@ -58,25 +58,28 @@ integerIndex = find(booleanIndex);
 subplot(1,2,1);imshow(queryImage);title('Query Face');
 subplot(1,2,2);imshow(read(training(integerIndex),1));title('Matched Class');
 
-%% Test First 5 People from Test Set
+%% Test 2 times people from Test Set
 figure;
 figureNum = 1;
-for person=1:5
-    for j = 1:test(person).Count
+for person=1:12
+    %for j = 1:test(person).Count
+     for j = 10:11 %chose sample number
         queryImage = read(test(person),j);
         queryFeatures = extractHOGFeatures(queryImage);
         personLabel = predict(faceClassifier,queryFeatures);
         % Map back to training set to find identity
         booleanIndex = strcmp(personLabel, personIndex);
         integerIndex = find(booleanIndex);
+        
         subplot(2,2,figureNum);imshow(imresize(queryImage,3));title('Query Face');
         subplot(2,2,figureNum+1);imshow(imresize(read(training(integerIndex),1),3));title('Matched Class');
+       
         figureNum = figureNum+2;
-        
-    end
+        %%export_fig(sprintf('Resultados/plot%d.png', person)); used only
+        %%local to save images for report.
+  end
     figure;
     figureNum = 1;
-
 end
 
 
